346d2ff0b28861057b17f2e7321584262ac5ca1c
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2017-03-30 10:14:11 +0100

    make new inventory request - apply new inventory request to inventories controller

--- app/Http/Controllers/API/V1/InventoriesController.php
+++ app/Http/Controllers/API/V1/InventoriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Request\NewInventoryRequest;
 
 class InventoriesController extends Controller
 {
@@ -23,7 +24,7 @@ class InventoriesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(NewInventoryRequest $request)
     {
         //
     }
