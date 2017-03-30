43d40c036fef35e4fcc26acdb8ba4844471793fb
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2017-03-30 09:47:42 +0100

    create inventory model

---
+++ app/Inventory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $table = 'inventories';
+
+    protected $fillable = [
+    	'name', 'description'
+    ];
+}
