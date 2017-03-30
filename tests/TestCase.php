0f77e0bef49d400fe60692ab475eb0696d1b6967
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2017-03-30 10:27:52 +0100

    setup test database and testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp() {
+    	parent::setUp();
+
+    	Artisan::call('migrate');
+
+    	$this->truncateAll();
+    }
+
+    public function truncateAll() {
+    	collect(['inventories'])->each(function ($table) {
+    		\DB::table($table)->truncate();
+    	});
+    }
 }
