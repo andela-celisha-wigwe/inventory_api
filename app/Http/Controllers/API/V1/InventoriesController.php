9f863bd7b7aa867a1fc8d2fef392e2503172e39f
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2017-03-30 09:57:18 +0100

    create inventories controller

---
+++ app/Http/Controllers/API/V1/InventoriesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class InventoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
